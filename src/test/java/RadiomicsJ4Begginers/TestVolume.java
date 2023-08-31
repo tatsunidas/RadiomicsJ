b50492bc55b53774d91eaec06f907f8f024da185
tatsunidas <tatsunidas@tatsunidas-Galago-Pro>
2023-08-31 12:00:47 +0900

    add externals.

---
+++ src/test/java/RadiomicsJ4Begginers/TestVolume.java
@@ -0,0 +1,22 @@
+package RadiomicsJ4Begginers;
+
+import features.MorphologicalFeatureType;
+import features.MorphologicalFeatures;
+import features.TestDataLoader;
+import ij.ImagePlus;
+
+public class TestVolume {
+
+	public static void main(String[] args) {
+		//
+		ImagePlus[] imgAndMask = TestDataLoader.digital_phantom1();
+		MorphologicalFeatures molph = new MorphologicalFeatures(imgAndMask[0], imgAndMask[1], 1);
+		
+		Double volumeMesh = molph.calculate(MorphologicalFeatureType.VolumeMesh.id());
+		System.out.println("Volume (Mesh):" + volumeMesh);
+		
+		Double volumeVoxelCounting = molph.calculate(MorphologicalFeatureType.VolumeVoxelCounting.id());
+		System.out.println("Volume (Voxel Counting):" + volumeVoxelCounting);
+	}
+
+}
